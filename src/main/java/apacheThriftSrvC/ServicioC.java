4737b7050f6a398d3d1a601bf045ab0d453558bc
Pedro F. <pedro.alonso.garcia@accenture.com>
2016-07-11 13:43:28 +0200

    Socket 9095 (srvB --> srvC)

--- src/main/java/apacheThriftSrvC/ServicioC.java
+++ src/main/java/apacheThriftSrvC/ServicioC.java
@@ -84,7 +84,7 @@ public class ServicioC {
 			TThreadPoolServer.Args serverArgs = new TThreadPoolServer.Args(serverTransport);
 			serverArgs.processor(processor);
 			TServer server =  new TThreadPoolServer(serverArgs);*/
-			TServerTransport serverTransport = new TServerSocket(9093);
+			TServerTransport serverTransport = new TServerSocket(9095);
 			TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport)
 			            .processor(processor).protocolFactory(new TJSONProtocol.Factory())
 			            .inputTransportFactory(new TFramedTransport.Factory())
